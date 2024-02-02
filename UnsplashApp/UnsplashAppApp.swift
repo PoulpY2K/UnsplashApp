43447dabfbaa3c692801f39368b8c69726ad4870
Jérémy Laurent <poulpy2k@gmail.com>
2024-02-02 08:49:07 +0100

    Initial Commit

---
+++ UnsplashApp/UnsplashAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashAppApp.swift
+//  UnsplashApp
+//
+//  Created by Jérémy Laurent on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
